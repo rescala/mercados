433c2365839e0e65b14cda2605f0439531a25ad6
Luis Rescala <luis.rescala@gmail.com>
2022-07-15 13:20:45 -0500

    15-julio-keys

--- src/keys.js
+++ src/keys.js
@@ -2,8 +2,8 @@ module.exports ={
     database:{
         host:'localhost',
         user:'root',
-        //password:'Passw.123',
-        password:'',
+        password:'Passw.123',
+        //password:'',
         database:'mercados'
     }
 };
